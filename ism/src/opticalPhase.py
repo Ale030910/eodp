c0f5b07a94c5c262c51d7e3f85365d5ccf32f71c
Ale0309 <alejandraitur@gmail.com>
2026-09-17 20:24:03 +0200

    17 de septiembre

--- ism/src/opticalPhase.py
+++ ism/src/opticalPhase.py
@@ -115,6 +115,26 @@ class opticalPhase(initIsm):
         :return: TOA image 2D in radiances [mW/m2]
         """
         # TODO
+
+        isrf, wv_isrf = readIsrf(self.auxdir + '/' + self.ismConfig.isrffile, band)
+
+        #0. init output
+        toa=np.zeros((sgm_toa.shape[0], sgm_toa.shape[1]))
+        #1. normalis ISRF
+        isrf = isrf / np.sum(isrf)
+
+        # 2. convertimos la wavelentgh en nm multiplicando por 1000
+        wv_isrf = wv_isrf*1000 #nm
+
+        # create interpolant of the ISRF - interp ISRF to the sgm wavelengths
+
+        #interp_isrf = cs(sgm_wv)  # 1D vector
+
+        for ialt in range(sgm_toa.shape[0]):
+            for iact in range(sgm_toa.shape[1]):
+                cs = interp1d(sgm_wv, sgm_toa[ialt,iact,:], fill_value=(0, 0), bounds_error=False)
+                sgm_inter = cs(wv_isrf)
+
+                toa[ialt, iact] = np.sum(sgm_inter*isrf)
+
         return toa
-
-
